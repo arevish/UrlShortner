f226df01db953aed3aa84ec028d222b65fba005e
arevish <vishalvk386879@gmail.com>
2025-06-05 18:59:04 +0530

    add file for url shortner working

---
+++ Controllers/url.js
@@ -0,0 +1,30 @@
+import shortid from "shortid";
+import { Url } from "../Models/Url.js"
+
+export const shortUrl = async (req,res)=>{
+    const longUrl = req.body.longUrl;
+    const shortCode = shortid.generate();
+
+    const shortUrl = `http://localhost:1000/${shortCode}`
+
+    //save to database 
+    const newUrl = new Url({shortCode , longUrl})
+    await newUrl.save();
+
+    console.log("short url saved" , newUrl);
+    res.render("index.ejs",{shortUrl})
+
+}
+
+export const getOriginalUrl = async( req, res)=>{
+    const shortCode = req.params.shortCode
+    //finf oon database 
+    const originalUrl = await Url.findOne({shortCode})
+    
+    if(originalUrl){
+        res.redirect(originalUrl.longUrl)
+    }else{
+
+        res.json({message:'Invalid shortcode', shortcode:shortCode})
+    }
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+import express  from "express";
+import mongoose from "mongoose";
+import { getOriginalUrl, shortUrl } from "./Controllers/url.js";
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://vishalvk386879:MvrYgA30LjD7ADy4@cluster0.ngluz7v.mongodb.net/",
+    {dbName:"NodeTest"}
+).then(()=> console.log("MongoDb Connected !!!"))
+.catch((err)=>console.log(err))
+
+//render ejs file 
+app.get('/', (req,res)=>{
+    res.render("index.ejs" , {shortUrl : null})
+})
+
+//shorting url logic 
+app.post("/short",shortUrl);
+
+//redirect to orignal url
+app.get("/:shortCode",getOriginalUrl);
+
+const port = 1000;
+app.listen(port , ()=> console.log(`server is running on port ${port}`))
